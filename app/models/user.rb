88ee7709ceedf712de5eb26327497d83cf18c845
kenta0905 <77146336+kenta0905@users.noreply.github.com>
2021-03-17 16:18:33 +0900

    ranking_usersテーブル 作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
-
   validates :nickname, presence: true
+
+  has_many :ranking_users
+  has_many :rankings, through: :ranking_users
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'votes/index'
   root to: "votes#index"
   resources :users, only: [:edit, :update]
+  resources :rankings, only: [:new, :create]
 end
